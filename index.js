81faba16377e79e29d3a4d2c8b1de0f280656888
Ingrid Voldmo <ingridvoldmo@Ingrid-sin-MacBook-Air.local>
2025-09-09 15:32:37 +0200

    FIX: added new URL for spear, still missing error handling

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ async function getTools() {
     //endre til dean sin ppt med catch og Promises, eller legg inn try catch
   const response = await fetch('https://island-survival-kit-builder.onrender.com/tools');
   tools = await response.json(); //extract JSON from the http response
+  tools[2]["img-url"] = "https://cdn.glitch.global/4ab89df4-051b-4f6c-b564-2317ed5cc084/Spear.png";
  setDropdownOptions();
  
   
@@ -22,8 +23,8 @@ function setDropdownOptions(){
     const dropdown = document.getElementById("myDropdown");
     for (const tool of tools) {
     const option = document.createElement("option");
-    option.value = tool.id;      // verdien som sendes ved valg
-    option.textContent = tool.title; // teksten som vises
+    option.value = tool.id;
+    option.textContent = tool.title; 
     dropdown.appendChild(option);
   }
 }
